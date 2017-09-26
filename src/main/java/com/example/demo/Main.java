5a3a478f8209918805930cf1bb7a98d8b07f77b3
Max Tweddell <maxt.nz@gmail.com>
2017-09-26 16:04:43 +1300

    added user input

--- src/main/java/com/example/demo/Main.java
+++ src/main/java/com/example/demo/Main.java
@@ -28,6 +28,57 @@ public class Main extends GameApplication {
                 .buildAndAttach(getGameWorld());
 
     }
+
+    @Override
+    protected void initInput() {
+        Input input = getInput(); // get input service
+
+        input.addAction(new UserAction("Move Right") {
+            @Override
+            protected void onAction() {
+                player.translateX(5); //move right 5 pixels
+                getGameState().increment("pixelsMoved", +5);
+            }
+        }, KeyCode.D);
+
+        input.addAction(new UserAction("Move Left") {
+            @Override
+            protected void onAction() {
+                player.translateX(-5); //move right 5 pixels
+                getGameState().increment("pixelsMoved", +5);
+            }
+        }, KeyCode.A);
+
+        input.addAction(new UserAction("Move Down") {
+            @Override
+            protected void onAction() {
+                player.translateY(5); //move right 5 pixels
+                getGameState().increment("pixelsMoved", +5);
+            }
+        }, KeyCode.S);
+
+        input.addAction(new UserAction("Move Up") {
+            @Override
+            protected void onAction() {
+                player.translateY(-5); //move right 5 pixels
+                getGameState().increment("pixelsMoved", +5);
+            }
+        }, KeyCode.W);
+
+    }
+
+    @Override
+    protected void initUI() {
+        Text textPixels = new Text();
+        textPixels.setTranslateX(50);
+        textPixels.setTranslateY(100);
+        getGameScene().addUINode(textPixels);
+    }
+
+    @Override
+    protected void initGameVars(Map<String, Object> vars) {
+        vars.put("pixelsMoved", 0);
+    }
     public static void main(String[] args) {
         launch(args);
     }
